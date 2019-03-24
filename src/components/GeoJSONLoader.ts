a886ffe3c67a1ce1b5919c252e269e8abca8a540
Russell Porter <contact@russellporter.com>
2019-03-24 23:27:10 +0100

    Update GeoJSON path

--- src/components/GeoJSONLoader.ts
+++ src/components/GeoJSONLoader.ts
@@ -3,15 +3,15 @@ import { Feature, Geometry } from 'geojson';
 import { SkiLiftData, SkiRunData } from './MapData';
 
 export function loadRun(lid: string): Promise<Feature<Geometry, SkiRunData>> {
-  return load('runs', lid)
+  return load(lid)
 }
 
 export function loadLift(lid: string): Promise<Feature<Geometry, SkiLiftData>> {
-  return load('lifts', lid)
+  return load(lid)
 }
 
-function load(entityType: string, entityLID: string): Promise<Feature<Geometry, any>> {
-  return fetch('https://tiles.skimap.org/' + entityType + '/' + entityLID + '.geojson')
+function load(entityLID: string): Promise<Feature<Geometry, any>> {
+  return fetch('https://tiles.skimap.org/features/' + entityLID + '.geojson')
     .then(response => {
       return response.json();
     });
