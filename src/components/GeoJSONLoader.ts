e7aa53a766244b8146369f613adf7883af4e8923
Russell Porter <contact@russellporter.com>
2019-07-21 16:06:16 +0200

    Switch to new api for features

--- src/components/GeoJSONLoader.ts
+++ src/components/GeoJSONLoader.ts
@@ -2,7 +2,7 @@ import "whatwg-fetch";
 
 export function loadGeoJSON<T>(entityID: string): Promise<T> {
   return fetch(
-    "https://tiles.skimap.org/features/" + entityID + ".geojson"
+    "https://api.openskimap.org/features/" + entityID + ".geojson"
   ).then(response => {
     return response.json();
   });
