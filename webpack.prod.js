e9c8990b95c7d3fda5d0157b6aac8abbb171c68d
Russell Porter <contact@russellporter.com>
2024-11-18 22:12:13 +0100

    Add back service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const TerserPlugin = require("terser-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = merge(common, {
   optimization: {
@@ -12,6 +13,12 @@ module.exports = merge(common, {
   },
   plugins: [
     new BundleAnalyzerPlugin({ analyzerMode: "disabled" }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
     new webpack.DefinePlugin({
       ENABLE_SERVICE_WORKER: true,
     }),
