56993e28826b6c9c5eb3978954f581de086348e4
Russell Porter <contact@russellporter.com>
2025-07-13 09:47:17 +0200

    Add contours

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,3 +1,5 @@
+import mlcontour from "maplibre-contour";
+import { DemSource } from "maplibre-contour/dist/dem-source";
 import * as maplibregl from "maplibre-gl";
 import { throttle } from "throttle-debounce";
 import MapFilters from "../MapFilters";
@@ -31,6 +33,7 @@ export class Map {
 
   private interactionManager: MapInteractionManager;
   private filterManager: MapFilterManager;
+  private demSource: DemSource | null = null;
 
   constructor(
     center: maplibregl.LngLatLike,
@@ -85,40 +88,124 @@ export class Map {
 
     addUnitSystemChangeListener_NonReactive({
       onUnitSystemChange: (unitSystem) => {
-        this.updateContourLabelUnits(unitSystem);
+        this.waitForMapLoaded(() => {
+          this.updateContourLayers(unitSystem);
+        });
         this.updateScaleControlUnits(unitSystem);
       },
       triggerWhenInitialized: true,
     });
   }
 
-  private updateContourLabelUnits(unitSystem: UnitSystem) {
-    this.waitForMapLoaded(() => {
-      const contourLabel = this.map.getLayer("contour-label");
-      if (!contourLabel) {
-        return;
-      }
-      switch (unitSystem) {
-        case "imperial":
-          this.map.setLayoutProperty("contour-label", "text-field", [
-            "concat",
-            ["to-string", ["*", ["get", "ele"], 3.3]],
-            " ft",
-          ]);
-          break;
-        case "metric":
-          this.map.setLayoutProperty("contour-label", "text-field", [
-            "concat",
-            ["get", "ele"],
-            " m",
-          ]);
-          break;
-      }
-    });
+  private updateContourLayers(unitSystem: UnitSystem) {
+    if (!this.demSource) {
+      this.demSource = new mlcontour.DemSource({
+        url: "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png",
+        encoding: "terrarium",
+        maxzoom: 15,
+        worker: true,
+        cacheSize: 100,
+        timeoutMs: 10000,
+      });
+
+      this.demSource.setupMaplibre(maplibregl);
+    }
+
+    // If there is already a terrain source in the map style, it's updated to use the same source to avoid double loading.
+    const terrainSource: maplibregl.RasterDEMTileSource | undefined =
+      this.map.getSource("terrain");
+    if (terrainSource) {
+      terrainSource.setTiles([this.demSource.sharedDemProtocolUrl]);
+    }
+
+    const contourTiles = [
+      this.demSource.contourProtocolUrl({
+        multiplier: this.getContourMultiplier(unitSystem),
+        thresholds: {
+          11: [200, 1000],
+          12: [100, 500],
+          13: [20, 100],
+          15: [10, 100],
+        },
+        // Reduces the amount of tiles that need to be loaded and rendered.
+        overzoom: 1,
+        contourLayer: "contours",
+        elevationKey: "ele",
+        levelKey: "level",
+      }),
+    ];
+
+    const contourSource: maplibregl.VectorTileSource | undefined =
+      this.map.getSource("contours");
+    if (contourSource) {
+      // Update existing source with new tiles for unit system
+      contourSource.setTiles(contourTiles);
+    } else {
+      // Add new source
+      this.map.addSource("contours", {
+        type: "vector",
+        tiles: contourTiles,
+        maxzoom: 15,
+      });
+    }
+
+    if (!this.map.getLayer("contour-lines")) {
+      this.map.addLayer({
+        id: "contour-lines",
+        type: "line",
+        source: "contours",
+        "source-layer": "contours",
+        paint: {
+          "line-color": "rgba(0,0,0, 10%)",
+          "line-width": ["match", ["get", "level"], 1, 1, 0.5],
+        },
+      });
+    }
+
+    const contourLabelsLayer = this.map.getLayer("contour-labels");
+    if (contourLabelsLayer) {
+      // Update existing layer text field for unit system
+      this.map.setLayoutProperty(
+        "contour-labels",
+        "text-field",
+        this.getContourTextFieldExpression(unitSystem)
+      );
+    } else {
+      // Add new layer
+      this.map.addLayer({
+        id: "contour-labels",
+        type: "symbol",
+        source: "contours",
+        "source-layer": "contours",
+        filter: [">", ["get", "level"], 0],
+        paint: {
+          "text-halo-color": "white",
+          "text-halo-width": 1,
+        },
+        layout: {
+          "symbol-placement": "line",
+          "text-anchor": "center",
+          "text-size": 10,
+          "text-field": this.getContourTextFieldExpression(unitSystem),
+          "text-font": ["Noto Sans Regular"],
+        },
+      });
+    }
   }
 
   private updateScaleControlUnits(unitSystem: UnitSystem) {
     this.mapScaleControl.setUnit(unitSystem);
+  }
+
+  private getContourMultiplier(unitSystem: UnitSystem): number {
+    return unitSystem === "imperial" ? 3.28084 : 1;
+  }
+
+  private getContourTextFieldExpression(
+    unitSystem: UnitSystem
+  ): maplibregl.ExpressionSpecification {
+    const unit = unitSystem === "imperial" ? " ft" : " m";
+    return ["concat", ["get", "ele"], unit];
   }
 
   private waitForMapLoaded = (closure: () => void) => {
@@ -149,7 +236,7 @@ export class Map {
 
   setStyle = (style: MapStyle) => {
     this.map.once("style.load", () => {
-      this.updateContourLabelUnits(getUnitSystem_NonReactive());
+      this.updateContourLayers(getUnitSystem_NonReactive());
     });
 
     this.map.setStyle(style);
