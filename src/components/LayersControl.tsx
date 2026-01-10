8ad9484a57fb900f5cec68849dd6770a649aa0ca
Russell Porter <contact@russellporter.com>
2026-01-10 09:53:26 +0100

    Fix layers icon color on iOS

--- src/components/LayersControl.tsx
+++ src/components/LayersControl.tsx
@@ -20,7 +20,7 @@ export class LayersControl implements maplibregl.IControl {
     
     // Create SVG icon for layers
     this._button.innerHTML = `
-      <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+      <svg width="18" height="18" viewBox="0 0 24 24" fill="#333">
         <path d="M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27-7.38 5.74zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16zm0-11.47L17.74 9 12 13.47 6.26 9 12 4.53z"/>
       </svg>
     `;
