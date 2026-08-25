e18c8ab066a253c98f3176a1939910afa7fbeed0
Russell Porter <contact@russellporter.com>
2026-08-25 08:38:53 +0200

    New landing page for openskidata

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,4 @@
-import {
-  Link,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Link, Typography } from "@mui/material";
 import * as React from "react";
 import EventBus from "./EventBus";
 import { EmbedCode } from "./EmbedCode";
@@ -77,7 +68,11 @@ export default class About extends React.Component<Props> {
         </Typography>
         <Typography paragraph={true}>
           Direct use of tiles hosted at tiles.openskimap.org is not permitted.
-          Please prepare and host your own tiles using the data below instead.
+          Please prepare and host your own tiles using the data from{" "}
+          <Link href="https://openskidata.org" target="_blank">
+            openskidata.org
+          </Link>{" "}
+          instead.
         </Typography>
         <Typography variant="h6" id="modal-title">
           Code
@@ -96,83 +91,12 @@ export default class About extends React.Component<Props> {
           Data Downloads
         </Typography>
         <Typography paragraph={true}>
-          The following data is available for download under the license.
-          GeoJSON format is spatial data with all attributes, while CSV is
-          tabular data for easier analysis. Note that data formats are not
-          stable and may change as the project evolves.
-        </Typography>
-        <Typography paragraph={true}>
-          Use is subject to several licenses. You must attribute
-          OpenSkiMap.org, OpenStreetMap, and <Link
-            onClick={(e: any) => {
-              e.preventDefault();
-              this.props.eventBus.openLegal();
-            }}
-            href="#"
-          >terrain data providers</Link> appropriately.
-        </Typography>
-        <TableContainer sx={{ marginBottom: 2 }}>
-          <Table size="small">
-            <TableHead style={{ backgroundColor: "#f5f5f5" }}>
-              <TableRow>
-                <TableCell style={{ fontWeight: "bold" }}>Dataset</TableCell>
-                <TableCell style={{ fontWeight: "bold" }}>GeoJSON</TableCell>
-                <TableCell style={{ fontWeight: "bold" }}>CSV</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>Ski Areas</TableCell>
-                <TableCell>
-                  <Link href="https://tiles.openskimap.org/geojson/ski_areas.geojson">
-                    Download
-                  </Link>
-                </TableCell>
-                <TableCell>
-                  <Link href="https://tiles.openskimap.org/csv/ski_areas.csv">
-                    Download
-                  </Link>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Lifts</TableCell>
-                <TableCell>
-                  <Link href="https://tiles.openskimap.org/geojson/lifts.geojson">
-                    Download
-                  </Link>
-                </TableCell>
-                <TableCell>
-                  <Link href="https://tiles.openskimap.org/csv/lifts.csv">
-                    Download
-                  </Link>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Runs</TableCell>
-                <TableCell>
-                  <Link href="https://tiles.openskimap.org/geojson/runs.geojson">
-                    Download
-                  </Link>
-                </TableCell>
-                <TableCell>
-                  <Link href="https://tiles.openskimap.org/csv/runs.csv">
-                    Download
-                  </Link>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Typography paragraph={true}>
-          All datasets are also available in a single{" "}
-          <Link href="https://tiles.openskimap.org/openskidata.gpkg">
-            GeoPackage file
+          Ski area, run, lift and spot data is available for download in
+          GeoJSON, CSV and GeoPackage format at{" "}
+          <Link href="https://openskidata.org" target="_blank">
+            openskidata.org
           </Link>
-          .
-        </Typography>
-        <Typography paragraph={true}>
-          The data is generally updated daily. It's permitted to automatically
-          download the above files once per day.
+          , along with the schema and licensing details.
         </Typography>
       </>
     );
